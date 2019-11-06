10fa866f814224c13abc011e1e7afede469e9bb3
jose sandino <armandosanb@gmail.com>
2019-11-06 09:21:32 -0600

    Cerra sesion

--- app/Views/theme/lte/hearder.blade.php
+++ app/Views/theme/lte/hearder.blade.php
@@ -104,7 +104,7 @@
                   <a href="#" class="btn btn-default btn-flat">Perfil</a>
                 </div>
                 <div class="pull-right">
-                  <a href="/" class="btn btn-default btn-flat">Cerrar Sesion</a>
+                  <a href='/Codeigniter-RedesDeSolidaridad/public/' class="btn btn-default btn-flat">Cerrar Sesion</a>
                 </div>
               </li>
             </ul>
