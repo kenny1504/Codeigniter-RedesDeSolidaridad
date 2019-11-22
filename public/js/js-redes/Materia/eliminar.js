058ff504389c27cf9941f76b3591cb179ea2d9f1
jose sandino <armandosanb@gmail.com>
2019-11-21 22:49:43 -0600

    ultimo,(lo vuelo a subir porque estaba lo que habias puesto de alerta

--- public/js/js-redes/Materia/eliminar.js
+++ public/js/js-redes/Materia/eliminar.js
@@ -15,36 +15,20 @@ function eliminar(button)
     $('#id_materia').val(ide);   //manda id_materia "id" a ventana modal
 
 }
-<<<<<<< HEAD
 
-=======
-url="/elimina/asignatura"; //kenny
->>>>>>> de5f9b1fe286582d9a3010265cfd8723720f7b48
     $("#confirmar").click(function() {
         
          $.ajax({
                     type: 'POST',
-<<<<<<< HEAD
-                    url: 'asignatura/eliminar', // ruta eliminar materia
-                    data: $('#delete_materia').serialize(), // manda el form donde se encuentra la modal materia
-=======
                     url: 'elimina/asignatura', // ruta eliminar materia
                     data: $('#delete_Asignatura').serialize(), // manda el form donde se encuentra la modal materia
->>>>>>> de5f9b1fe286582d9a3010265cfd8723720f7b48
                     success: function(data){ 
-
-                    if(data==1)
-                    {
                         dat.remove(); //remueve la fila eliminado 
                         $("#exito").modal("show"); //abre modal de exito
                         $("#exito").fadeTo(2000,500).slideUp(450,function(){   // cierra la modal despues del tiempo determinado  
                                     $("#exito").modal("hide"); // cierra modal
                                     } );
-                    } 
-                    else
-                    {
-                        alert("imposible")
-                    }
+
                    
                     }
              });// 
